5904344536a2e49e32651709d394dd576cb7d755
AlienEngineer <Alien.Software.Engineer@gmail.com>
2014-11-17 15:09:18 +0000

    display github issues

--- Credential/TestApplication/Scripts/app.js
+++ Credential/TestApplication/Scripts/app.js
@@ -1,19 +1,26 @@
-﻿
-
-(function() {
-
-    var app = angular.module('app', []);
-
-    app.constant('GITHUB_API', 'https://api.github.com/');
+﻿'use strict';
 
 
-    app.directive('githubIssuesList', ['$http', 'GITHUB_API', 'toaster', function ($http, GITHUB_API, toaster) {
+angular.module('app', ['toaster', 'ngCookies', 'angular-loading-bar'])
+  .constant('GITHUB_API', 'https://api.github.com/')
+  .service('github_token', ['$cookies', function($cookies) {
+    return {
+      get: function() {
+        return angular.fromJson($cookies.Github).Access_Token;
+      }
+    }
+  }])
+  .directive('githubIssuesList', ['$http', 'GITHUB_API', 'toaster', 'github_token', function ($http, GITHUB_API, toaster, github_token) {
         return {
             templateUrl: 'Directives/github-issues-list.html',
             restrict: 'E',
             link: function(scope) {
 
-                $http.get(GITHUB_API + 'issues')
+                $http.get(GITHUB_API + 'issues', {
+                      headers: {
+                        authorization: 'token ' + github_token.get()
+                      }
+                    })
                     .success(function(data) {
                         scope.issues = data;
                     })
@@ -23,7 +30,4 @@
 
             }
         };
-    }]);
-
-
-})();
+    }])
